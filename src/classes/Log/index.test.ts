f0be4de2db1fcfcfd331114c29049e9bd4d26e87
Jason Matthew <jasonm4130@gmail.com>
2024-08-31 21:38:14 +1000

    fix: tidied up regex for matching log

--- src/classes/Log/index.test.ts
+++ src/classes/Log/index.test.ts
@@ -3,27 +3,35 @@ import { Log } from '.';
 describe('Log', () => {
   it('should parse the log string correctly', () => {
     const logString =
-      '127.0.0.1 - - [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326';
+      '50.112.00.11 - admin [11/Jul/2018:17:33:01 +0200] "GET /asset.css HTTP/1.1" 200 3574 "-" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6"';
     const log = new Log(logString);
 
-    expect(log.ip).toBe('127.0.0.1');
-    expect(log.timeString).toBe('[10/Oct/2000:13:55:36 -0700]');
-    expect(log.url).toBe('/apache_pb.gif');
+    expect(log.ip).toBe('50.112.00.11');
+    expect(log.timeString).toBe('11/Jul/2018:17:33:01 +0200');
+    expect(log.url).toBe('/asset.css');
   });
 
   it('should return a valid Date object from getTime', () => {
     const logString =
-      '127.0.0.1 - - [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326';
+      '50.112.00.11 - admin [11/Jul/2018:17:33:01 +0200] "GET /asset.css HTTP/1.1" 200 3574 "-" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6"';
     const log = new Log(logString);
     const date = log.getTime();
 
     expect(date).toBeInstanceOf(Date);
-    expect(date?.getFullYear()).toBe(2000);
-    expect(date?.getMonth()).toBe(9); // Months are zero-indexed in JavaScript
-    expect(date?.getDate()).toBe(10);
-    expect(date?.getHours()).toBe(13);
-    expect(date?.getMinutes()).toBe(55);
-    expect(date?.getSeconds()).toBe(36);
+    expect(date?.getFullYear()).toBe(2018);
+    expect(date?.getMonth()).toBe(6); // Months are zero-indexed in JavaScript
+    expect(date?.getDate()).toBe(11);
+    expect(date?.getHours()).toBe(17);
+    expect(date?.getMinutes()).toBe(33);
+    expect(date?.getSeconds()).toBe(1);
+  });
+
+  it('should work for ipv6 addresses', () => {
+    const logString =
+      '2001:0db8:85a3:0000:0000:8a2e:0370:7334 - admin [11/Jul/2018:17:33:01 +0200] "GET /asset.css HTTP/1.1" 200 3574 "-" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6"';
+    const log = new Log(logString);
+
+    expect(log.ip).toBe('2001:0db8:85a3:0000:0000:8a2e:0370:7334');
   });
 
   it('should return null for getTime if timeString is null', () => {
@@ -37,7 +45,7 @@ describe('Log', () => {
     const logString = 'invalid log string';
     const log = new Log(logString);
 
-    expect(log.ip).toBe('invalid');
+    expect(log.ip).toBeNull();
     expect(log.timeString).toBeNull();
     expect(log.url).toBeNull();
   });
--- src/classes/Log/index.ts
+++ src/classes/Log/index.ts
@@ -8,14 +8,18 @@ export class Log {
   url: string | null;
 
   constructor(log: string) {
-    const parts = log.split(' ');
-    this.ip = parts[0] || null;
-    if (parts[3] && parts[4]) {
-      this.timeString = parts[3] + ' ' + parts[4];
-    } else {
-      this.timeString = null;
-    }
-    this.url = parts[6] || null;
+    // The log line looks like:
+    // 50.112.00.11 - admin [11/Jul/2018:17:33:01 +0200] "GET /asset.css HTTP/1.1" 200 3574 "-" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6"
+    // Regex to match the ip portion of the log line (ipv4 and ipv6)
+    const regex =
+      /(?<ip>(?:\d{1,3}\.){3}\d{1,3}|[0-9a-fA-F:]+) - (?<userName>.+) \[(?<timestamp>\d{2}\/\w{3}\/\d{4}:?\d{2}:\d{2}:\d{2} \+?\d{4})\] "(?<method>GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH|) (?<path>\S+) (?<protocol>\S+)" (?<statusCode>\d{3}) (?<bytes>\d+) "(?<referrer>.+)" "(?<userAgent>.+)"$/;
+    const match = log.match(regex);
+
+    console.log(log);
+
+    this.ip = match?.groups?.ip || null;
+    this.timeString = match?.groups?.timestamp || null;
+    this.url = match?.groups?.path || null;
   }
 
   getTime() {
@@ -23,11 +27,36 @@ export class Log {
       return null;
     }
 
-    const formattedString = this.timeString
-      .replace(/[[\]-]/g, '')
-      .replace(/[/]/g, ' ')
-      .replace(/[:]/, ' ');
+    // The time string looks like this: 09/Jul/2018:15:48:20 +0200
+    const [day, month, year, hour, minute, second] =
+      this.timeString.split(/[:/ ]/);
 
-    return new Date(formattedString);
+    return new Date(
+      parseInt(year, 10),
+      this.getMonthIndex(month),
+      parseInt(day, 10),
+      parseInt(hour, 10),
+      parseInt(minute, 10),
+      parseInt(second, 10),
+    );
+  }
+
+  private getMonthIndex(month: string) {
+    const months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+
+    return months.indexOf(month);
   }
 }
